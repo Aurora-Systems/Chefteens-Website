d6e4b41752a7d8f15fd3bfe1b89261b1c27a7685
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2022-09-02 15:37:35 +0200

    changes

--- src/Pages/gallery.tsx
+++ src/Pages/gallery.tsx
@@ -1,21 +1,30 @@
 import cvideo from "../assets/vid/video.mp4"
+import {Fade} from "react-awesome-reveal"
 const Gallery=()=>{
     return(
         <div className="container">
             <div className="row text-center mt-2 d-flex align-items-center">
                 <div className="col-sm">
-                    <video src={cvideo} autoPlay loop controls width="100"  className="rounded"/>
+                    <Fade direction="left">
+                        <video src={cvideo} autoPlay loop controls width="100"  className="rounded"/>
+                    </Fade>
                 </div>
                 <div className="col-sm">
-                    <h1 className="display-1 animate-character">Gallery</h1>
+                    <Fade direction="down">
+                        <h1 className="display-1 animate-character">Gallery</h1>
+                    </Fade>
                 </div>
                 <div className="col-sm">
-                    <video src={cvideo} autoPlay loop controls width="100"  className="rounded"/>
+                    <Fade direction="right">
+                        <video src={cvideo} autoPlay loop controls width="100"  className="rounded"/>
+                    </Fade>
                 </div>
                 
             </div>
             <div className="">
-                <div className="powr-social-feed" id="3a57d853_1662121542"></div>
+                <Fade direction="down">
+                    <div className="powr-social-feed" id="3a57d853_1662121542"></div>
+                </Fade>
             </div>
             
         </div>
